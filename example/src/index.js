1b605093572d6907d0cacd2ba8f738a1be432c6b
Vikram Rao <vikram.s.rao@gmail.com>
2018-01-27 13:40:21 +0530

    Proposal creation form done

--- example/src/index.js
+++ example/src/index.js
@@ -1,7 +1,7 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { FormControl, FormGroup, HelpBlock, ControlLabel } from 'react-bootstrap';
+import { FormControl, FormGroup, HelpBlock, ControlLabel, Button } from 'react-bootstrap';
 
 class FormInput extends React.Component {
     render() {
@@ -20,6 +20,11 @@ ReactDOM.render(
         <h4>Create Proposal</h4>
         <FormInput label="Lending Token" placeholder="Enter the address" extraHelp="This is token address in hexadecimal like 0xABCDE..." />
         <FormInput label="Lending Quantity" placeholder="Enter the quantity" extraHelp="Ex: 20" />
+        <FormInput label="Desired Token" placeholder="Enter the address" extraHelp="This is token address in hexadecimal like 0xABCDE..." />
+        <FormInput label="Term" placeholder="Enter the term in days" extraHelp="Ex: 15" />
+        <FormInput label="Rate" placeholder="Enter the rate" extraHelp="How many desired tokens for 1 lending token" />
+        <FormInput label="Return Rate" placeholder="Enter the return rate" extraHelp="How many desired tokens for 1 lending token after contract term ends" />
+        <Button bsStyle="primary">Create</Button>
     </div>,
     document.getElementById('content')
 )
